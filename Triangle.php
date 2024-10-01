80b12d305052fe9b5fb42a8c5cce8e19a08143f1
DarkKnight8247 <franz.castor25@gmail.com>
2024-10-01 21:23:39 +0800

    Update Triangle.php

--- Triangle.php
+++ Triangle.php
@@ -6,8 +6,7 @@
     //calculate
     $area = 0.5 * $base * $height;
     $hypotenuse = sqrt(pow($base, 2) + pow($height, 2));
-    #..-. .-. .- -. --..
-
+    
     // display
     echo "Base: $base <br>";
     echo "Height: $height <br>";
