54981b4f8fd697da41ff573f28a5c131836fa011
DarkKnight8247 <franz.castor25@gmail.com>
2024-10-01 10:57:14 +0800

    Static Rectangle Calculator
    It calculates the area and perimeter of the length and width of a rectangle.

---
+++ Rectangle.php
@@ -0,0 +1,16 @@
+<?php
+    //variables
+    $length = 5;
+    $width = 6;
+
+    //calc
+    $area = $length * $width;
+    $perimeter = 2 * ($length + $width);
+    #..-. .-. .- -. --..
+
+    //display
+    echo "Length: $length <br>";
+    echo "Width: $width <br>"; 
+    echo "Area: $area <br>";
+    echo "Perimeter: $perimeter";
+?>
