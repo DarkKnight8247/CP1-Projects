a68432a0d5ef375602c61ee77877d9d9c03d4dc8
DarkKnight8247 <franz.castor25@gmail.com>
2024-10-01 11:03:38 +0800

    Static Circle Calculator
    It calculates the area and circumference of a circle based on its radius.

---
+++ Circle.php
@@ -0,0 +1,14 @@
+<?php
+    //var
+    $radius = 3;
+
+    //calc
+    $area = M_PI * pow($radius, 2);
+    $circumference = 2 * M_PI * $radius ;
+    #..-. .-. .- -. --..
+
+    //display fgsfwtwth
+    echo "The radius of a circle $radius is equal to:<br>";
+    echo "&nbsp;&nbsp;&nbsp;&nbsp;Area: "  . round($area, 2) . "<br>";
+    echo "&nbsp;&nbsp;&nbsp;&nbsp;Circumference: " . round($circumference, 2) . "<br>";
+    ?>
