3b397cd639f6e303077fe6d2c3db71d9f8f3758b
DarkKnight8247 <franz.castor25@gmail.com>
2024-10-31 18:33:10 +0800

    Lab4.php
    It detects the largest/greatest among 3 inputted numbers.

--- Lab4.php
+++ Lab4.php
@@ -10,6 +10,7 @@
         $num1 = $_POST["num1"];
         $num2 = $_POST["num2"];
         $num3 = $_POST["num3"];
+        #..-. .-. .- -. --.. 
 
         if( $num1 > $num2 && $num1 > $num3 ) {
             echo"$num1 is greater than $num2 and $num3";
